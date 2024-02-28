fb49a18528ce04dd1cc041bdf9eec91a2ea4ac4b
Gabriel Ortner <ortner.gabriel@gmail.com>
2024-02-29 00:06:39 +0100

    - Added a command-line parameter to load a project directly on startup. - Removed the test-data from the code

--- Executioner/App.xaml.cs
+++ Executioner/App.xaml.cs
@@ -9,6 +9,16 @@ namespace Executioner
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            string? filename = null;
+            if (e.Args.Length == 1)
+                filename = e.Args[0];
+
+            MainWindow wnd = new(filename);
+
+            wnd.Show();
+        }
     }
 
 }
