fd528aacdd7f61f23db16e089a3f7a115e9c5d22
Gabriel Ortner <ortner.gabriel@gmail.com>
2024-03-05 20:38:55 +0100

    Added a simple FileChooser custom-control

---
+++ Executioner/Controls/FileChooserEdit.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Executioner.Controls
+{
+    /// <summary>
+    /// Interaction logic for FileChooserEdit.xaml
+    /// </summary>
+    public partial class FileChooserEdit : UserControl
+    {
+        public string Text
+        {
+            get
+            {
+                return FileChooserTextBox.Text;
+            }
+            set
+            {
+                FileChooserTextBox.Text = value;
+            }
+        }
+        public FileChooserEdit()
+        {
+            InitializeComponent();
+        }
+
+        public void OnFileChooserButtonClick(object sender, RoutedEventArgs e)
+        {
+            OpenFolderDialog openFolderDialog = new OpenFolderDialog();
+            if (Text.Length > 0)
+                openFolderDialog.InitialDirectory = Text;
+
+            if (openFolderDialog.ShowDialog() == true)
+                Text = openFolderDialog.FolderName;
+        }
+    }
+}
