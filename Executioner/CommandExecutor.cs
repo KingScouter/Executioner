b328d9d5da94d066bb39fc3a9ec7561636b5c935
Gabriel Ortner <ortner.gabriel@gmail.com>
2024-04-15 23:26:10 +0200

    Added a simple documentation to CommandExecutor

--- Executioner/CommandExecutor.cs
+++ Executioner/CommandExecutor.cs
@@ -6,8 +6,17 @@ using System.Windows;
 
 namespace Executioner
 {
+    /// <summary>
+    /// Executor to execute a given command (parse template, user-input, execution)
+    /// </summary>
     internal class CommandExecutor
     {
+        /// <summary>
+        /// Execute a command
+        /// </summary>
+        /// <param name="commandData">Command data to execute</param>
+        /// <param name="additionalArguments">Additional arguments to use instead of user-input</param>
+        /// <param name="dryRun">True to only print the command, otherwise false to execute it</param>
         public static void ExecuteCommand(CommandData commandData, List<string> additionalArguments, bool dryRun)
         {
             string parsedCommand = ParseCommand(commandData, additionalArguments);
@@ -20,6 +29,12 @@ namespace Executioner
             ExecuteCommandInternal(parsedCommand, commandData);
         }
 
+        /// <summary>
+        /// Parse a command. Execute the user-input for the set parameters.
+        /// </summary>
+        /// <param name="commandData">Data of the command to parse</param>
+        /// <param name="additionalArguments">List of arguments to use before any user-input</param>
+        /// <returns>Parsed command as string</returns>
         static string ParseCommand(CommandData commandData, List<string> additionalArguments)
         {
             CommandTemplate commandTemplate = commandData.CommandTemplate;
@@ -38,15 +53,23 @@ namespace Executioner
             return sb.ToString().Trim();
         }
 
-        static string ParseTemplateElement(string templateElement, Dictionary<string, IBaseUserInputParameter> parameters, List<string> additionalArguments)
+        /// <summary>
+        /// Parse a single template-element. Check the parameter-data from the stored command and open the input-window.
+        /// </summary>
+        /// <param name="keyword">Keyword of the parameter</param>
+        /// <param name="parameters">List of defined parameters for the command</param>
+        /// <param name="additionalArguments">List of arguments to use instead of a user-input</param>
+        /// <returns>Parsed value as a string for the parsed command</returns>
+        /// <exception cref="ArgumentException"></exception>
+        static string ParseTemplateElement(string keyword, Dictionary<string, IBaseUserInputParameter> parameters, List<string> additionalArguments)
         {
             string preSetValue = additionalArguments.FirstOrDefault("");
             if (additionalArguments.Count > 0)
                 additionalArguments.RemoveAt(0);
 
-            IBaseUserInputParameter? param = parameters[templateElement];
+            IBaseUserInputParameter? param = parameters[keyword];
             if (param == null)
-                throw new ArgumentException($"Param {templateElement} is not defined");
+                throw new ArgumentException($"Param {keyword} is not defined");
 
             if (preSetValue != "")
                 return preSetValue;
@@ -54,6 +77,12 @@ namespace Executioner
             return ExecuteParameter(param);
         }
 
+        /// <summary>
+        /// Open the input-window for user-input parameters
+        /// </summary>
+        /// <param name="param">Parameter to let the user enter</param>
+        /// <returns>Parsed value of the parameter for the parsed command</returns>
+        /// <exception cref="ArgumentException">User cancel</exception>
         private static string ExecuteParameter(IBaseUserInputParameter param)
         {
             InputParameterWindow paramWindow = new(param);
@@ -63,6 +92,11 @@ namespace Executioner
             return paramWindow.OutputData!;
         }
 
+        /// <summary>
+        /// Determine the type of command and execute it accordingly
+        /// </summary>
+        /// <param name="parsedCommand">Command string to execute</param>
+        /// <param name="data">Command data</param>
         private static void ExecuteCommandInternal(string parsedCommand, CommandData data)
         {
             try
@@ -82,6 +116,13 @@ namespace Executioner
                 MessageBox.Show(ex.Message, "Error");
             }
         }
+
+        /// <summary>
+        /// Execute a command in the windows command-line
+        /// </summary>
+        /// <param name="commandTemplate">Command template</param>
+        /// <param name="waitForResult">Flag to determine if the CLI should remain open after the execution finished</param>
+        /// <param name="workingDir">Working directory</param>
         private static void ExecuteCmdCommand(string commandTemplate, Boolean waitForResult, string workingDir)
         {
             System.Diagnostics.ProcessStartInfo startInfo = new()
@@ -100,6 +141,12 @@ namespace Executioner
             ExecuteProcess(startInfo);
         }
 
+        /// <summary>
+        /// Execute a command in the windows PowerShell
+        /// </summary>
+        /// <param name="commandTemplate">Command template</param>
+        /// <param name="waitForResult">Flag to determine if the CLI should remain open after the execution finished</param>
+        /// <param name="workingDir">Working directory</param>
         private static void ExecutePowershellCommand(string commandTemplate, bool waitForResult, string workingDir)
         {
             System.Diagnostics.ProcessStartInfo startInfo = new()
@@ -119,6 +166,10 @@ namespace Executioner
             ExecuteProcess(startInfo);
         }
 
+        /// <summary>
+        /// Internal method to startup and execute a process for the command
+        /// </summary>
+        /// <param name="startInfo">Process info for startup</param>
         private static void ExecuteProcess(System.Diagnostics.ProcessStartInfo startInfo)
         {
             System.Diagnostics.Process process = new System.Diagnostics.Process();
