18836766c57e76e815088812c02a851c96516a43
Gabriel Ortner <ortner.gabriel@gmail.com>
2024-04-17 23:26:07 +0200

    Created a textbox that can be configured to only allow specific characters

---
+++ Executioner/Controls/AdvancedTextInputBox.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Executioner.Controls
+{
+    public enum TextInputBoxMode
+    {
+        All,
+        CharOnly,
+        NumberOnly
+    }
+
+    /// <summary>
+    /// Interaction logic for AdvancedTextInputBox.xaml
+    /// </summary>
+    public partial class AdvancedTextInputBox : TextBox
+    {
+        public bool AllowWhitespace { get; set; } = true;
+
+        public TextInputBoxMode Mode
+        {
+            get { return mode; }
+            set
+            {
+                switch (value)
+                {
+                    case TextInputBoxMode.CharOnly:
+                        formatRegex = CharOnlyRegex();
+                        break;
+                    case TextInputBoxMode.NumberOnly:
+                        formatRegex = NumberOnlyRegex();
+                        break;
+                    case TextInputBoxMode.All:
+                    default:
+                        formatRegex = AllRegex();
+                        break;
+                }
+
+                mode = value;
+            }
+        }
+
+
+        [GeneratedRegex(".+")]
+        private static partial Regex AllRegex();
+
+        [GeneratedRegex("\\w+")]
+        private static partial Regex CharOnlyRegex();
+
+        [GeneratedRegex("\\d+")]
+        private static partial Regex NumberOnlyRegex();
+
+        private TextInputBoxMode mode = TextInputBoxMode.All;
+        private Regex formatRegex = AllRegex();
+
+        public AdvancedTextInputBox()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        
+        private bool IsTextAllowed(string text)
+        {
+            return formatRegex.IsMatch(text);
+        }
+
+        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (!AllowWhitespace && e.Key == Key.Space)
+                e.Handled = true;
+        }
+    }
+}
