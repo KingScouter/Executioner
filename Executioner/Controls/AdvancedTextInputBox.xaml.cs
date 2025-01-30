7c9a6b1794f9d714c30ecf7fb07e8e12af2e4928
Gabriel Ortner <ortner.gabriel@gmail.com>
2025-01-30 22:30:12 +0100

    - Added the input-mode for the AdvancedTextInputBox as a DependencyProperty - Expanded the regex for number-inputs to allow real numbers

--- Executioner/Controls/AdvancedTextInputBox.xaml.cs
+++ Executioner/Controls/AdvancedTextInputBox.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Text.RegularExpressions;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Executioner.Controls
 {
@@ -16,6 +18,19 @@ namespace Executioner.Controls
     /// </summary>
     public partial class AdvancedTextInputBox : TextBox
     {
+        public static readonly DependencyProperty ModeProperty =
+        DependencyProperty.RegisterAttached("Mode", typeof(TextInputBoxMode), typeof(AdvancedTextInputBox), new PropertyMetadata(default(TextInputBoxMode)));
+
+        public static void SetMode(UIElement element, TextInputBoxMode value)
+        {
+            element.SetValue(ModeProperty, value);
+        }
+
+        public static TextInputBoxMode GetMode(UIElement element)
+        {
+            return (TextInputBoxMode)element.GetValue(ModeProperty);
+        }
+
         public bool AllowWhitespace { get; set; } = true;
 
         public TextInputBoxMode Mode
@@ -30,6 +45,7 @@ namespace Executioner.Controls
                         break;
                     case TextInputBoxMode.NumberOnly:
                         formatRegex = NumberOnlyRegex();
+                        AllowWhitespace = false;
                         break;
                     case TextInputBoxMode.All:
                     default:
@@ -48,20 +64,27 @@ namespace Executioner.Controls
         [GeneratedRegex("\\w+")]
         private static partial Regex CharOnlyRegex();
 
-        [GeneratedRegex("\\d+")]
+        [GeneratedRegex("^(?:-(?:[1-9](?:\\d{0,2}(?:,\\d{3})+|\\d*))|(?:0|(?:[1-9](?:\\d{0,2}(?:,\\d{3})+|\\d*))))(?:.\\d+|)$")]
         private static partial Regex NumberOnlyRegex();
 
         private TextInputBoxMode mode = TextInputBoxMode.All;
         private Regex formatRegex = AllRegex();
 
+        private Brush defaultForeground;
+
         public AdvancedTextInputBox()
         {
             InitializeComponent();
+
+            Loaded += AdvancedTextInputBox_Loaded;
         }
 
         private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        private void AdvancedTextInputBox_Loaded(object sender, RoutedEventArgs e)
         {
-            e.Handled = !IsTextAllowed(e.Text);
+            Mode = AdvancedTextInputBox.GetMode(this);
+            defaultForeground = Foreground;
+            Loaded -= AdvancedTextInputBox_Loaded;
         }
 
         
